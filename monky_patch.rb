3bc5b5d0d147d1c748f1d04552af037afd623cdb
moguno <moguno@Alicia.local>
2013-09-08 21:15:20 +0900

    安定版？で読み込めなくなっていたのを修正

--- monky_patch.rb
+++ monky_patch.rb
@@ -3,8 +3,8 @@
 class Gtk::MikutterWindow
   alias :initialize_org :initialize
 
-  def initialize(imaginally, plugin, *args)
-    initialize_org(imaginally, plugin, *args)
+  def initialize(imaginally, *args)
+    initialize_org(imaginally, *args)
     self.decorated = false
   end
 
